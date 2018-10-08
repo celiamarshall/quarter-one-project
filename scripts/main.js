fccffbadfac5ff59288b18f2447606d3f8a89e0e
Celia Marshall <celia.m@gmail.com>
2018-10-08 10:59:31 -0700

    each fact family cycles through all 11 facts

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
 const functions = require('./functions') 
 
-let timesTable = []
-
 const quizBox = document.querySelector('.quiz-box')
 const families = document.querySelectorAll('.fact-family')
+const inputAnswer = document.querySelector('#answer')
+let familyPickedBool = false
+let timesTable = []
+
 
 for (family of families) {
     family.addEventListener('click', (event) => {
+        familyPickedBool = true
+        inputAnswer.focus()
         indicator.textContent = ''
         if (event.target.textContent[1] === '0') {
             timesTable = functions.makeTimesTable(10)
@@ -14,16 +18,19 @@ for (family of families) {
         else {
             timesTable = functions.makeTimesTable(Number(event.target.textContent[0]))
         }
-        quizBox.textContent = timesTable[Math.floor(Math.random()*timesTable.length)];
+        let randomIndex = Math.floor(Math.random()*timesTable.length)
+        quizBox.textContent = timesTable[randomIndex]
+        timesTable.splice(randomIndex, 1)
+        //event.target.style.backgroundColor = "#BF0808"
     })
 }
 
 const form = document.querySelector('form')
-const inputAnswer = document.querySelector('#answer')
 const indicator = document.querySelector('.indicator')
 const starsOnSite = document.querySelector('.star-number')
 const trackerBox = document.querySelector('.tracker-box')
 let starCount = Number(localStorage.getItem('stars'))
+let waitingToType = false
 
 if (starCount){
     starsOnSite.textContent = starCount
@@ -31,20 +38,32 @@ if (starCount){
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    if (!quizBox.textContent){
+
+    //if user has not started quiz, or they have just finished a quiz
+    if (!quizBox.textContent || quizBox.textContent === 'Nice work!'){
+        quizBox.textContent = ''
+        inputAnswer.value = ''
         indicator.textContent = 'Pick a fact family!'
         indicator.style.color = 'black'
     }
+    //if user sees a question, but did not enter answer
     else if (!inputAnswer.value){
         indicator.textContent = 'Oops! Type in your answer.'
         indicator.style.color = 'blue'
+        waitingToType = true
     }
     else {
         const bool = functions.checkAnswer(quizBox.textContent, inputAnswer.value)
         inputAnswer.value = ''
+
         if (bool) {
             indicator.textContent = 'Correct!'
             indicator.style.color = 'green'
+            waitingToType = false
+            setTimeout(() => {
+                indicator.textContent = ''
+            }, 1000)
+
             if (starCount){
                 starCount++
                 localStorage.setItem('stars', starCount)
@@ -54,10 +73,31 @@ form.addEventListener('submit', (event) => {
             }
             starsOnSite.textContent = starCount
         }
+
         else {
-            indicator.textContent = 'Try Again!'
+            indicator.textContent = 'Next time!'
             indicator.style.color = 'red'
+            waitingToType = false
+            setTimeout(() => {
+                indicator.textContent = ''
+            }, 1000)
         }
+    }
+
+    //if there are still questions to be asked in the timesTable array, but we are not waiting for the user to type in their answer
+    if (timesTable.length > 0 && !waitingToType) {
+        setTimeout(() => { 
+            randomIndex = Math.floor(Math.random()*timesTable.length)
+            quizBox.textContent = timesTable[randomIndex]
+            timesTable.splice(randomIndex, 1)
+        }, 1000)
+    }
+
+    //if a fact family has been picked and a user has entered input for all of the questions
+    else if (familyPickedBool && !waitingToType){
+        quizBox.textContent = 'Nice work!'
+        indicator.textContent = ''
+        familyPickedBool = false
     }
 })
 
